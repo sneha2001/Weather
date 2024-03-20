cd6f73ab76ac7bc1b44b0884d2cb19f13609ad02
Sneha harigharun <109866539+sneha2001@users.noreply.github.com>
2024-03-20 12:24:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+
+let weather = {
+  apiKey: "aba6ff9d6de967d5eac6fd79114693cc",
+  fetchWeather: function (city) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data))
+      .catch((error) => {
+        console.error("Error fetching weather:", error.message);
+        alert("Error fetching weather.");
+      });
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    const city = document.querySelector(".search-bar").value;
+    if (city) {
+      this.fetchWeather(city);
+    } else {
+      alert("Please enter a city.");
+    }
+  },
+};
+
+let geocode = {
+  reverseGeocode: function (latitude, longitude) {
+    var apikey = "90a096f90b3e4715b6f2e536d934c5af";
+
+    var api_url = "https://api.opencagedata.com/geocode/v1/json";
+
+    var request_url =
+      api_url +
+      "?" +
+      "key=" +
+      apikey +
+      "&q=" +
+      encodeURIComponent(latitude + "," + longitude) +
+      "&pretty=1" +
+      "&no_annotations=1";
+
+    fetch(request_url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Unable to reverse geocode.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const city = data.results[0].components.city;
+        weather.fetchWeather(city);
+        console.log(city);
+      })
+      .catch((error) => {
+        console.error("Error reverse geocoding:", error.message);
+      });
+  },
+  getLocation: function () {
+    function success(data) {
+      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, console.error);
+    } else {
+      weather.fetchWeather("chennai");
+    }
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    weather.search();
+  }
+});
+
+weather.fetchWeather("chennai");
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    weather.search();
+  }
+});
+
+geocode.getLocation();
